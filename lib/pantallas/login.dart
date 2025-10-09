3deb875e17647e95e6224b26ec2fb59c0f29d811
LilianaGuadalupeDJM1 <lilianadjmartinez07@gmail.com>
2025-10-09 16:28:01 -0600

    arreglo en la app

--- lib/pantallas/login.dart
+++ lib/pantallas/login.dart
@@ -225,6 +225,7 @@ class _LoginPageState extends State<LoginPage> {
         },
         body: jsonEncode(body),
       );
+      print(response);
 
       // Verificamos si la respuesta es exitosa
       if (response.statusCode == 200) {
